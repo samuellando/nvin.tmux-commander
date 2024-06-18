6f0d3d843f71ab0c5c7ef7aecffc270fc327a8a3
Samuel Lando <samuel.lando@jeppesen.com>
2024-06-18 18:27:21 +0000

    Convert to tmux plugin

--- Makefile
+++
@@ -1,9 +0,0 @@
-fmt:
-	echo "===> Formatting"
-	stylua lua/ --config-path=.stylua.toml
-
-lint:
-	echo "===> Linting"
-	luacheck lua/ --globals vim
-
-pr-ready: fmt lint
--- lua/commander/tmux.lua
+++ lua/commander/tmux.lua
@@ -1,24 +1,8 @@
-local harpoon = require("harpoon")
-local log = require("harpoon.dev").log
-local global_config = harpoon.get_global_settings()
+local log = require("commander.dev").log
 local utils = require("harpoon.utils")
 
 local M = {}
 local tmux_windows = {}
-
-if global_config.tmux_autoclose_windows then
-    local harpoon_tmux_group = vim.api.nvim_create_augroup(
-        "HARPOON_TMUX",
-        { clear = true }
-    )
-
-    vim.api.nvim_create_autocmd("VimLeave", {
-        callback = function()
-            require("harpoon.tmux").clear_all()
-        end,
-        group = harpoon_tmux_group,
-    })
-end
 
 local function create_terminal(name)
     log.trace("tmux: _create_terminal())")
@@ -110,16 +94,6 @@ local function find_terminal(args)
     end
 end
 
-local function get_first_empty_slot()
-    log.trace("_get_first_empty_slot()")
-    for idx, cmd in pairs(harpoon.get_term_config().cmds) do
-        if cmd == "" then
-            return idx
-        end
-    end
-    return M.get_length() + 1
-end
-
 function M.gotoTerminal(idx)
     log.trace("tmux: gotoTerminal(): Window:", idx)
     local window_handle = find_terminal(idx)
@@ -140,14 +114,6 @@ function M.sendCommand(idx, cmd, ...)
     log.trace("tmux: sendCommand(): Window:", idx)
     local window_handle = find_terminal(idx)
 
-    if type(cmd) == "number" then
-        cmd = harpoon.get_term_config().cmds[cmd]
-    end
-
-    if global_config.enter_on_sendcmd then
-        cmd = cmd .. "\n"
-    end
-
     log.error("ID", window_handle.window_id)
 
     if cmd then
@@ -167,76 +133,12 @@ function M.sendCommand(idx, cmd, ...)
     end
 end
 
-function M.clear_all()
-    log.trace("tmux: clear_all(): Clearing all tmux windows.")
-
-    for _, window in pairs(tmux_windows) do
-        -- Delete the current tmux window
-        utils.get_os_command_output({
-            "tmux",
-            "kill-window",
-            "-t",
-            window.window_id,
-        }, vim.loop.cwd())
-    end
-
-    tmux_windows = {}
-end
-
-function M.get_length()
-    log.trace("_get_length()")
-    return table.maxn(harpoon.get_term_config().cmds)
-end
-
-function M.valid_index(idx)
-    if idx == nil or idx > M.get_length() or idx <= 0 then
-        return false
-    end
-    return true
-end
-
-function M.emit_changed()
-    log.trace("_emit_changed()")
-    if harpoon.get_global_settings().save_on_change then
-        harpoon.save()
-    end
-end
-
-function M.add_cmd(cmd)
-    log.trace("add_cmd()")
-    local found_idx = get_first_empty_slot()
-    harpoon.get_term_config().cmds[found_idx] = cmd
-    M.emit_changed()
-end
-
-function M.rm_cmd(idx)
-    log.trace("rm_cmd()")
-    if not M.valid_index(idx) then
-        log.debug("rm_cmd(): no cmd exists for index", idx)
-        return
-    end
-    table.remove(harpoon.get_term_config().cmds, idx)
-    M.emit_changed()
-end
-
-function M.set_cmd_list(new_list)
-    log.trace("set_cmd_list(): New list:", new_list)
-    for k in pairs(harpoon.get_term_config().cmds) do
-        harpoon.get_term_config().cmds[k] = nil
-    end
-    for k, v in pairs(new_list) do
-        harpoon.get_term_config().cmds[k] = v
-    end
-    M.emit_changed()
-end
-
 function M.create_terminal(name)
     log.trace("create_terminal()", name)
     if not terminal_exists(name) then
         create_terminal(name)
         return true
     end
-    M.emit_changed()
     return false
 end
 
